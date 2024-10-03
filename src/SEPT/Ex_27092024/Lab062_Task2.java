50ead3333db28ebfc2f5277b4ef8429c001bb848
Sharma <shikha.an.sharma@accenture.com>
2024-10-03 21:33:15 +0530

    Update

--- src/SEPT/Ex_27092024/Lab062_Task2.java
+++ src/SEPT/Ex_27092024/Lab062_Task2.java
@@ -3,12 +3,12 @@ package SEPT.Ex_27092024;
 public class Lab062_Task2 {
     //Create a Java program that prints the first 5 even numbers using a do-while loop.
     public static void main(String[] args) {
-        int a=1;
+        int num=0;
+        int count=0;
         do {
-            if(a%2==0)
-            System.out.println(a);
-            a++;
-
-        }while(a<=10);
+            System.out.println(num);
+            num+=2;
+            count++;
+        }while(count<5);
     }
 }
